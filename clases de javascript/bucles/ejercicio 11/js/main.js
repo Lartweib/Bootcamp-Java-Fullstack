483208e20c332941e44c41cfdaa43876e728ba46
lartweib <a.federicodaniel@gmail.com>
2022-04-21 12:55:02 +0200

    ejercicio 11

---
+++ clases de javascript/bucles/ejercicio 11/js/main.js
@@ -0,0 +1,6 @@
+// Ejercicio 11
+
+// Entra en el if ya que le definimos la variable i con valor
+// al principio.
+// Su comportamiento cambia una vez ya ha entrado en el if.
+// Mostrara por pantalla la variable numero y a partir de ahi solo entrara en el else que le sumara. un numero cada vez hasta que el numero sea mayor que 5
