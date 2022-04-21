4d7b4930c4d6b57f57a456945a20768975446cc2
lartweib <a.federicodaniel@gmail.com>
2022-04-21 09:52:54 +0200

    bucles

---
+++ clases de javascript/bucles/ejercicios 09/js/main.js
@@ -0,0 +1,12 @@
+// Ejercicio 08-1
+console.log("Bucle for:");;
+for (let i=0; i<10; i++) {
+  console.log(i+1 + ". I ❤️ code");
+}
+
+console.log("\nBucle while:");;
+let num=0;
+while (num < 10) {
+  console.log(num+1 + ". I ❤️ code");
+  num++;
+}
