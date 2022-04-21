4ed9370b9f562d514c5ab9d6b7e7365a72cc0127
lartweib <a.federicodaniel@gmail.com>
2022-04-21 11:17:21 +0200

    bucle

---
+++ clases de javascript/bucles/ejercicio 10 for each/js/main.js
@@ -0,0 +1,13 @@
+// Ejercicio 10
+
+let miArray = [ "a", "b", "c", "d", "e" ];
+// let reversed = miArray.reverse();
+miArray.forEach( function(valor, indice) {
+    console.log("En el índice " + indice + " hay este valor: " + valor);
+});
+
+
+for(let numero=5; numero>0; numero--){
+    console.log(miArray[numero-1])
+}
+
