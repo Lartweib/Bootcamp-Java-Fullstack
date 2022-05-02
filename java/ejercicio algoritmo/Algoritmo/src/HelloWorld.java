60301a1ba88dc109a420ec38a8fffc9fa0c2215d
lartweib <a.federicodaniel@gmail.com>
2022-05-02 17:24:47 +0200

    update algoritmo

---
+++ java/ejercicio algoritmo/Algoritmo/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		int num1=10; //declara y asigna variable
+		int num2=20; //declara y asigna variable
+		int suma = num1 + num2; //declara y asigna la suma de variables
+		System.out.println(suma);//imprime por consola la variable suma
+	}
+}
