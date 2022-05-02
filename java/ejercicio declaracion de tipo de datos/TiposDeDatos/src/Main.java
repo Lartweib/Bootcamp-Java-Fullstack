08a0ca312145a65bce3a189d242c3551a3b55790
lartweib <a.federicodaniel@gmail.com>
2022-05-02 18:35:21 +0200

    update tipos de datos

---
+++ java/ejercicio declaracion de tipo de datos/TiposDeDatos/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+	public static void main (String[] args) {
+		//Logicos
+		System.out.println("Logicos:");
+		boolean bol1= true;
+		System.out.println("Booleano: "+bol1);
+		//Caracter
+		System.out.println("Caracter:");
+		char char1= 14;
+		System.out.println("Char: "+char1);
+		//Numericos enteros
+		System.out.println("Enteros:");
+		byte byte1=120;
+		System.out.println("Byte: "+byte1);
+		short short1=32700;
+		System.out.println("Short: "+short1);
+		int int1=2147483647;
+		System.out.println("Int: "+int1);
+		long long1=9223372036854775807l;
+		System.out.println("Long: "+long1);
+		//Numerico Reales
+		System.out.println("Reales:");
+		float float1=3.14f;
+		System.out.println("Float: "+float1);
+		double double1=17.898985458715;
+		System.out.println("Double: "+double1);
+	}
+}
