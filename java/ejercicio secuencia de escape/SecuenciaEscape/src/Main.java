046e0db7df0cb8ff1178273dec3808bee22a6ffe
lartweib <a.federicodaniel@gmail.com>
2022-05-02 17:16:45 +0200

    update secuencias de escape

---
+++ java/ejercicio secuencia de escape/SecuenciaEscape/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		//Salto de linea '\n'
+		System.out.print("Esto es un salto de linea-> \n");
+		//Tabulador '\t'
+		System.out.println("Esto es un tabulado->\t:)");
+		//Diagonal Inversa '\\'
+		System.out.println("Con esto podemos imprimir barra inversa->\\");
+		//Comillas Dobles '\"'
+		System.out.println("Asi imprimimos las-> \"comillas dobles\"");
+		//Comilla simple '\''
+		System.out.println("Y asi las-> \'comillas simples\'");
+		//Retorno de carro (solo modo admin) '\r'
+		System.out.println("Esto es un retorno de carro->\r (?)");
+		//Borrado a la izquierda (solo modo admin) '\b'
+		System.out.println("Y esto un borrado a la izquierda ->\b");
+	}
+}
