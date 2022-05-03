2e23bf9b6e8eb9056da691f6861cf91410c527ac
lartweib <a.federicodaniel@gmail.com>
2022-05-03 13:24:17 +0200

    update

---
+++ java/operador condicional/OperadorCondicional/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	static void parImpar(int numero) {
+		if ((numero%2)== 0) {
+			System.out.println("El numero es par");
+		}else {System.out.println("El numero es impar");}
+		
+	};
+	
+	public static void main(String[] args) {
+		parImpar(5);
+	}
+
+}
