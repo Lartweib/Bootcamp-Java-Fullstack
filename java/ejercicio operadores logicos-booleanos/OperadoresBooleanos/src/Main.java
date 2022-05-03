dd0d7bd31a2ce42af3626e94d293f3ba13f19dd3
lartweib <a.federicodaniel@gmail.com>
2022-05-03 11:40:52 +0200

    update

---
+++ java/ejercicio operadores logicos-booleanos/OperadoresBooleanos/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		boolean bool1=true;
+		boolean bool2=false;
+		boolean bool3=false;
+		System.out.println("Es bool1 = a el inverso de bool2?:" + (bool1 == !bool2));
+		System.out.println("Es bool2 = a el inverso de bool3?:" + (bool2 == !bool3));
+	}
+}
