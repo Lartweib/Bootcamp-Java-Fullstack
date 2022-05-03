1409f301423dc70b209c495f8919fd604005bdda
lartweib <a.federicodaniel@gmail.com>
2022-05-03 10:56:03 +0200

    update

---
+++ java/operadores aritmeticos/OperadoresAritmeticos/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {		
+		System.out.println("10 - 5 = " + (10 - 5) );
+		System.out.println("55 + 45 = " + (55 + 45) );
+		System.out.println("3 en negativo es: " + (-3) );
+		System.out.println("5 x 5= " + (5*5) );
+		System.out.println("El resultado entero de 25 / 7 es: " + (25/7) );
+		float resultado = (float)25/7;
+		System.out.println("El resultado real de 25 / 7 es: " + (resultado) );
+		System.out.println("El resto de 25 / 4 es: " + (25%4) );
+
+	}
+
+}
