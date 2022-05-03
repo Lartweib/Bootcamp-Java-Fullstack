a095ea3164afd09ab5adbede611097a688494e13
lartweib <a.federicodaniel@gmail.com>
2022-05-04 00:11:18 +0200

    update

--- java/estructuras condicionales/EstructurasCondicionales/src/Ejercicio5.java
+++ java/estructuras condicionales/EstructurasCondicionales/src/Ejercicio5.java
@@ -8,14 +8,14 @@ public class Ejercicio5 {
 	static void arbolito() {
         int i,j,k,n;
         n=10; 
-        for(i=1; i<n+(n/2); i++){
+        for(i=1; i<n+(n/2)-2; i++){
             for(j=n+(n/2); j>i; j--){
                 System.out.print(" ");}
             for(k=1; k<=2*i-1; k++){
                 System.out.print("*");}
             System.out.println("");
         }
-        for(i=1; i<n-(n/2); i++){
+        for(i=1; i<n-(n/2)-1; i++){
             for(j=n+(n/2)-2; j>1; j--){
                 System.out.print(" ");}
             for(k=n/2; k<=(n/2)+4; k++){
