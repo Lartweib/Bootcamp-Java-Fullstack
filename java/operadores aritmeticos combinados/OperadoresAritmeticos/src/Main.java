4be01bae2824448b9125acc2f49b73473f374c75
lartweib <a.federicodaniel@gmail.com>
2022-05-03 11:50:09 +0200

    update

---
+++ java/operadores aritmeticos combinados/OperadoresAritmeticos/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 25;
+		System.out.println("El valor de num es: " + num);
+		//1 
+		num += 5;
+		System.out.println("Suma combinada de 5: Num = " + num);
+		//2
+		num -= 15;
+		System.out.println("Resta combinada de 15: Num = " + num);
+		//3
+		num *= 2;
+		System.out.println("Producto combinada de 2: Num = " + num);
+		//4
+		num /= 2;
+		System.out.println("Division combinada de 2: Num = " + num);
+		//5
+		num %= 3;
+		System.out.println("Resto combinado de 3: Num = " + num);
+		
+		
+	}
+
+}
