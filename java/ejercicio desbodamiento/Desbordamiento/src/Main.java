7e2eeba7a18c1d3bed77719bf6e479759e6f4ead
lartweib <a.federicodaniel@gmail.com>
2022-05-03 09:48:01 +0200

    update

---
+++ java/ejercicio desbodamiento/Desbordamiento/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Byte
+		byte byte1=(byte) 132;
+		System.out.println(byte1);
+		//Short
+		short short1= (short) 32792;
+		System.out.println(short1);
+		//Int
+		int int1= (int) 2147493647l;
+		System.out.println(int1);
+		//Long
+		long long1= (long) 9223372036874775.807;
+		System.out.println(long1);
+	}
+
+}
