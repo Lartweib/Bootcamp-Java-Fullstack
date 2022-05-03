7f91124c510f0c0fd8206b5020f098f95e88945b
lartweib <a.federicodaniel@gmail.com>
2022-05-03 10:00:10 +0200

    update

---
+++ java/ejercicios constantes/Main/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+		//Byte
+		final byte byte1=(byte) 132;
+		System.out.println(byte1);
+		//Short
+		final short short1= (short) 32792;
+		System.out.println(short1);
+		//Int
+		final int int1= (int) 2147493647l;
+		System.out.println(int1);
+		//Long
+		final long long1= (long) 9223372036874775.807;
+		System.out.println(long1);
+		
+	}
+
+}
