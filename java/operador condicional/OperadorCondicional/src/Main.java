6f9c69b28bef960e33248436b84e174ea9f2c953
lartweib <a.federicodaniel@gmail.com>
2022-05-03 13:34:47 +0200

    update

--- java/operador condicional/OperadorCondicional/src/Main.java
+++ java/operador condicional/OperadorCondicional/src/Main.java
@@ -5,13 +5,20 @@ public class Main {
 		if ((numero%2)== 0) {
 			System.out.println("El numero es par");
 		}else {System.out.println("El numero es impar");}
-		
+	};
+	
+	static void blackWhite() {
+		int random= (int) Math.round(Math.random());
+		if (random== 0) {
+			System.out.println("Blanco");
+		}else {System.out.println("Negro");}
 	};
 	
 	public static void main(String[] args) {
 		int num = (int) (Math.random()*100+1);
 		System.out.println("El numero elegido es: "+num);
 		parImpar(num);
+		blackWhite();
 	}
 
 }
